d0a4706e3bef4242b94d7f2e6087defefa03432e
leonel47 <waboleonel@gmail.com>
2021-03-06 22:50:15 -0500

    Fetching data remotely

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import "./App.css";
 import AppBody from "./components/Body";
 import Header from "./components/common/Header";
 import List from "./components/list/List";
+import "./index.css";
 
 function App() {
   return (
     <div className="App">
-      <Header title={"React Application"} />
-      <AppBody />
+      <Header title={"Crypto-Currencies"} />
+      {/* <AppBody /> */}
       <List />
     </div>
   );
--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -2,8 +2,33 @@ import React, { Component } from "react";
 
 class List extends Component {
   state = {
-    loading: true,
+    loading: false,
+    currencies: [],
+    error: null,
   };
+  componentDidMount() {
+    this.setState({
+      loading: true,
+    });
+    fetch("https://api.udilia.com/coins/v1/cryptocurrencies?page=1&perPage=20")
+      .then((response) => {
+        return response.json().then((json) => {
+          return response.ok ? json : Promise.reject(json);
+        });
+      })
+      .then((data) => {
+        this.setState({
+          currencies: data.currencies,
+          loading: false,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          error: error.errorMessage,
+          loading: false,
+        });
+      });
+  }
   render() {
     if (this.state.loading) {
       return (
@@ -14,7 +39,34 @@ class List extends Component {
         </div>
       );
     }
-    return <div className="container">Listing</div>;
+    return (
+      <div className="container">
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Market Cap</th>
+              <th>Change Percentage (24H)</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.currencies.map((currency) => {
+              return (
+                <tr key={currency.rank}>
+                  <td>{currency.rank}</td>
+                  <td>{currency.name}</td>
+                  <td>{currency.price}</td>
+                  <td>{currency.marketCap}</td>
+                  <td>{currency.percentChange24h}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
 
