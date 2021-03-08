e1071b46997644a2035af3bf3801136da7e37c7c
leonel47 <waboleonel@gmail.com>
2021-03-08 08:36:17 -0500

    Ending implementing pagination

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Pagination = (props) => {
     const {page, totalPages, handlePagination} = props
@@ -6,16 +7,25 @@ const Pagination = (props) => {
         <div
             style={{textAlign: 'center'}}>
             <button
-                className={page === 1 ? "btn btn-secondary btn-sm mr-2" : "btn btn-primary btn-sm mr-2"}
+                className={page <= 1 ? "btn btn-secondary btn-sm mr-2" : "btn btn-primary btn-sm mr-2"}
                 onClick={() => handlePagination('prev')}
+                disabled={page <= 1}
             >&larr;</button>
             <span>Page: <b>{page}</b> of <b>{totalPages}</b></span>
             <button
-                className="btn btn-primary btn-sm ml-2"
+                className={page === totalPages ? "btn btn-secondary btn-sm ml-2" : "btn btn-primary btn-sm ml-2"}
                 onClick={() => handlePagination('next')}
+                disabled={page === totalPages}
             >&rarr;</button>
         </div>
     )
+}
+
+// Constraining state data elements
+Pagination.propTypes = {
+    page: PropTypes.number.isRequired,
+    totalPages: PropTypes.number.isRequired,
+    handlePagination: PropTypes.func.isRequired
 }
 
 export default Pagination
