437d4e71b180de142bdb3774ab8685c6f77b6cbc
leonel47 <waboleonel@gmail.com>
2021-03-08 10:14:03 -0500

    Start routing elements

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import "./App.css";
 import Header from "./components/common/Header";
-import List from "./components/list/List";
-// import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import "./index.css";
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import React from "react";
+import List from "./components/list/List";
 
 function App() {
     return (
-        <div className="App">
-            <Header title={"Crypto-Currencies"}/>
-            <List/>
-        </div>
+        <BrowserRouter>
+            <>
+                <Header title={"Crypto-Currencies"}/>
+                <Switch>
+                    <Route path="/currencies" component={List} exact/>
+                </Switch>
+            </>
+        </BrowserRouter>
     );
 }
 
